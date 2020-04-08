4f63c5e823f240bed4b6049add3325c14ad05094
GigiCoh <61231117+GigiCoh@users.noreply.github.com>
2020-04-08 14:59:09 +0800

    Add files via upload

---
+++ exercise.js
@@ -0,0 +1,64 @@
+function myFunction(){
+	var letter = document.getElementById("myInput").value;
+	var text;
+
+	if(letter === "a"){
+		text = "Correct!";
+	}else{
+		text = "Wrong! Please try again";
+	}
+
+	document.getElementById("answer").innerHTML = text;
+}
+
+function myFunction2(){
+	var letter = document.getElementById("myInput2").value;
+	var text;
+
+	if(letter === "c"){
+		text = "Correct!";
+	}else{
+		text = "Wrong! Please try again";
+	}
+
+	document.getElementById("answer2").innerHTML = text;
+}
+
+function myFunction3(){
+	var letter = document.getElementById("myInput3").value;
+	var text;
+
+	if(letter === "b"){
+		text = "Correct!";
+	}else{
+		text = "Wrong! Please try again";
+	}
+
+	document.getElementById("answer3").innerHTML = text;
+}
+
+function myFunction4(){
+	var letter = document.getElementById("myInput4").value;
+	var text;
+
+	if(letter === "b"){
+		text = "Correct!";
+	}else{
+		text ="Wrong! Please try again";
+	}
+
+	document.getElementById("answer4").innerHTML = text;
+}
+
+function myFunction5(){
+	var letter = document.getElementById("myInput5").value;
+	var text;
+
+	if(letter === "a"){
+		text = "Correct!";
+	}else{
+		text = "Wrong! Please try again";
+	}
+
+	document.getElementById("answer5").innerHTML = text;
+}
---
+++ project.js
@@ -0,0 +1,5 @@
+function welcome();
+
+function welcome(){
+	prompt('Please enter your name:');
+}
